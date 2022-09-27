91f83d1a11420f9f80ff59b7e8d52ae72dda54c6
shamskhodary <shams.khodary@gmail.com>
2022-09-27 13:41:08 +0300

    create landing page and style it relates #19

--- client/src/components/landingPage/LandingPage.jsx
+++ client/src/components/landingPage/LandingPage.jsx
@@ -1,12 +1,17 @@
 import CardContainer from './Card/CardContainer';
 import Aside from './Filter/Aside';
+import Header from './Header';
 
 function LandingPage() {
   return (
-    <div>
-      <Aside />
-      <CardContainer />
-    </div>
+    <>
+      <Header />
+      <section className="container">
+        <Aside />
+        <CardContainer />
+      </section>
+
+    </>
   );
 }
 
