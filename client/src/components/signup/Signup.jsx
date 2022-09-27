2b3ed6889c105fcb9530df4d2eb509581737acbd
shamskhodary <shams.khodary@gmail.com>
2022-09-27 15:27:22 +0300

    Handled signup and login fetching using axios and redirect the user into the landing page relates #25

--- client/src/components/signup/Signup.jsx
+++ client/src/components/signup/Signup.jsx
@@ -1,35 +1,64 @@
 /* eslint-disable react/button-has-type */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import '../../style/signup.css';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import asos from './Asos-black-logo.png';
 
 function Signup() {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [submit, setSubmit] = useState(false);
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!submit) return;
+
+    axios({
+      method: 'POST',
+      url: '/api/v1/signup',
+      data: {
+        email,
+        username,
+        password,
+        confirmPassword,
+      },
+    }).then((res) => {
+      if (res.status === 200) {
+        navigate('/');
+      }
+    }).catch((err) => console.log(err));
+  }, [submit]);
+
   return (
     <div className="main">
-      <img className="logo" src={asos} alt="Asos-black-logo" />
-      <form className="form" action="">
+      <Link to="/"><img className="logo" src={asos} alt="Asos-black-logo" /></Link>
+      <div className="form">
         <div className="headerForm">
-          <a href="/" className="check seconde">JOIN</a>
-          <a href="/" className="">SIGN IN</a>
+          <Link to="//" className="check seconde">JOIN</Link>
+          <Link to="/login" className="">SIGN IN</Link>
         </div>
         <div className="inputs">
 
           <label htmlFor="email">EMAIL ADDRESS:</label>
-          <input type="email" id="email" name="email" />
+          <input onChange={(e) => setEmail(e.target.value)} type="email" id="email" name="email" />
 
           <label htmlFor="username">USERNAME</label>
-          <input type="text" id="username" name="username" />
+          <input onChange={(e) => setUsername(e.target.value)} type="text" id="username" name="username" />
 
           <label htmlFor="password">PASSWORD:</label>
-          <input type="password" id="password" name="password" />
+          <input onChange={(e) => setPassword(e.target.value)} type="password" id="password" name="password" />
 
-          <label htmlFor="password">CONFIRM PASSWORD:</label>
-          <input type="password" id="password" name="password" />
+          <label htmlFor="confirmPassword">CONFIRM PASSWORD:</label>
+          <input onChange={(e) => setConfirmPassword(e.target.value)} type="password" id="confirmPassword" name="password" />
 
           <a href="/">Already have an account ?</a>
-          <button>SIGN UP</button>
+          <button onClick={() => { setSubmit(true); }}>SIGN UP</button>
         </div>
-      </form>
+      </div>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,23 @@ import {
   RouterProvider,
 } from 'react-router-dom';
 import LandingPage from './components/landingPage/LandingPage';
-import './style/card.css';
+import Signup from './components/signup/Signup';
+import Login from './components/login/Login';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <LandingPage />,
   },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
